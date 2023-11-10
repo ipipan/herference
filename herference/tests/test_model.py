997f1f4a9a84b202e37ba83fc9906ee7461bc38e
Karol Saputa <ksaputa@gputrain.dariah.ipipan.waw.pl>
2023-11-10 14:02:16 +0100

    feat: add performance prints

--- herference/tests/test_model.py
+++ herference/tests/test_model.py
@@ -1,9 +1,17 @@
 import unittest
+import time
 
 from herference import Herference, api
 
 
 class ModelTests(unittest.TestCase):
+    def setUp(self):
+        self.startTime = time.time()
+
+    def tearDown(self):
+        t = time.time() - self.startTime
+        print('%s: %.3f' % (self.id(), t))
+
     def test_create_model(self):
         manager = Herference()
         self.assertTrue(manager)
@@ -14,6 +22,18 @@ class ModelTests(unittest.TestCase):
         print(rt)
         self.assertTrue(isinstance(rt, api.Text))
 
+    def test_medium_prediction(self):
+        manager = Herference()
+        rt = manager.predict(r"Ala ma mruczącego kota, jest on bardzo ładny." * 100)
+        #print(rt)
+        self.assertTrue(isinstance(rt, api.Text))
+    
+    def test_long_prediction(self):
+        manager = Herference()
+        rt = manager.predict(r"Ala ma mruczącego kota, jest on bardzo ładny." * 1_000)
+        #print(rt)
+        self.assertTrue(isinstance(rt, api.Text))
 
 if __name__ == '__main__':
-    unittest.main()
+    suite = unittest.TestLoader().loadTestsFromTestCase(ModelTests)
+    unittest.TextTestRunner(verbosity=0).run(suite)
