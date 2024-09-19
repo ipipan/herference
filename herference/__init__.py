657eda2f584946f0f044f80265e2b063b96ffc05
Ka Saputa <ksaputa@Kas-MacBook-Air.local>
2024-09-19 14:35:43 +0200

    fixup! refactor(manager): move parser to omit torch import

--- herference/__init__.py
+++ herference/__init__.py
@@ -1,8 +1,14 @@
+import os
+import logging
+
+if os.getenv('HERFERENCE_LAZY_IMPORT') != '1':
+    from herference.manager import Herference
+    from herference.coref_sp import make_herference
+    logging.warning('HERFERENCE_LAZY_IMPORT')
+
+
 from pathlib import Path
 from spacy.util import load_model_from_init_py
-
-from herference.manager import Herference
-from herference.coref_sp import make_herference
 
 
 def load(**overrides):
